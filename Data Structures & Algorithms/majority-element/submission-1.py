791614de63eaa4a48fb493e27387331a10f4aad2
Mohak Kapoor <mohak.kapoor.v@gmail.com>
2026-05-30 02:14:19 +0530

    Add: majority-element - submission-1

---
+++ Data Structures & Algorithms/majority-element/submission-1.py
@@ -0,0 +1,9 @@
+
+class Solution:
+    def majorityElement(self, nums: List[int]) -> int:
+        length = len(nums)
+        c = Counter(nums)
+        for k,v in c.items():
+            if v > length/2:
+                return k
+        
