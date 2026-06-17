598e615799e520c319a91dfa5901d7fbe46d272f
Mohak Kapoor <mohak.kapoor.v@gmail.com>
2026-06-17 23:42:50 +0530

    Add: permutation-string - submission-3

---
+++ Data Structures & Algorithms/permutation-string/submission-3.py
@@ -0,0 +1,16 @@
+class Solution:
+    def checkInclusion(self, s1: str, s2: str) -> bool:
+        if len(s1)>len(s2): return False
+        freq = [0]*26
+        freq2 = [0]*26
+        for i in s1:
+            freq[ord(i)-ord('a')] += 1
+        for i in s2[:len(s1)]:
+            freq2[ord(i)-ord('a')] +=1
+
+        for i in range(len(s1),len(s2)):
+            if freq == freq2:
+                return True
+            freq2[ord(s2[i-len(s1)])-ord('a')] -= 1
+            freq2[ord(s2[i])-ord('a')] += 1
+        return freq == freq2
