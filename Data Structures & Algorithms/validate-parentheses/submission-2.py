e329e626b26df33d646bb3086f1d759ad9b3c441
Mohak Kapoor <mohak.kapoor.v@gmail.com>
2026-07-09 23:58:40 +0530

    Add: validate-parentheses - submission-2

---
+++ Data Structures & Algorithms/validate-parentheses/submission-2.py
@@ -0,0 +1,21 @@
+class Solution:
+    def isValid(self, s: str) -> bool:
+        arr = []
+        
+        d = {
+            ')': '(',
+            '}': '{',
+            ']': '[',
+        }
+        for i in s:
+            if i in list(d.keys()):
+                if len(arr)>0 and d[i] == arr[-1]:
+                    arr.pop()
+                else:
+                    return False
+            else:
+                arr.append(i)
+
+    
+        return True if len(arr) == 0 else False
+
