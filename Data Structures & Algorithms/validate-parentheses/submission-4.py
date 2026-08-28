36801eda314ef6ad3ab7740f7167144c42592649
Mohak Kapoor <mohak.kapoor.v@gmail.com>
2026-08-28 11:31:50 +0530

    Add: validate-parentheses - submission-4

---
+++ Data Structures & Algorithms/validate-parentheses/submission-4.py
@@ -0,0 +1,18 @@
+class Solution:
+    def isValid(self, s: str) -> bool:
+        brackets = {
+            '}' : '{',
+            ')' : '(',
+            ']' : '['
+        }
+        st = []
+        for i in s:
+            if i in brackets:
+                if st and st[-1] == brackets[i]:
+                    st.pop()
+                else:
+                    return False
+            else:
+                st.append(i)
+
+        return True if not st else False
