300968687164cc9954082f762ad5d9ba5d2fb9a0
Mohak Kapoor <mohak.kapoor.v@gmail.com>
2026-09-07 02:43:16 +0530

    Add: is-anagram - submission-6

---
+++ Data Structures & Algorithms/is-anagram/submission-6.py
@@ -0,0 +1,9 @@
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        if len(s) != len(t):
+            return False
+        c1,c2 = [0]*26,[0]*26
+        for i in range(len(s)):
+            c1[ord(s[i])-ord('a')] +=1
+            c2[ord(t[i])-ord('a')] +=1
+        return c1 == c2
